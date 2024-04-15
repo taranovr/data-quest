e267ef4889fa0f6961bfa897e66ccfdef264c475
Roman Taranov <Roman.Taranov@LiveNation.com>
2024-04-15 15:04:15 -0400

    updated notes & gitignore & dockerignore

--- lambda_function.py
+++ lambda_function.py
@@ -17,7 +17,7 @@ from config import (
 
 pd.set_option("display.float_format", lambda x: "%.2f" % x)
 
-## Part 1: AWS S3 & Sourcing Datasets
+# Part 1: AWS S3 & Sourcing Datasets
 s3_resource = boto3.resource("s3")
 
 
@@ -42,6 +42,7 @@ def handler(event, context):
     parse_api.store_api_response(API_LINK, s3_resource, S3_BUCKET_NAME)
     utils.sync_s3(s3_resource, S3_BUCKET_NAME)
 
+    # 3.0 Load both csv and json files
     part1_file = os.path.join(INPUT_PATH, "pr.data.0.Current")
     df_part1 = pd.read_csv(part1_file, delimiter="\t")
 
@@ -57,7 +58,9 @@ def handler(event, context):
     df_part2 = df_part2.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     df_part2.columns = df_part2.columns.str.replace(" ", "")
 
-    ### 3.1 Using the dataframe from the population data API (Part 2), generate the mean and the standard deviation of the annual US population across the years [2013, 2018] inclusive.
+    # 3.1 Using the dataframe from the population data API (Part 2), 
+    # generate the mean and the standard deviation of the annual US population 
+    # across the years [2013, 2018] inclusive.
 
     df2_calculation = df_part2[df_part2["IDYear"].between(2013, 2018)]
 
@@ -67,7 +70,10 @@ def handler(event, context):
     logger.info(f"The mean is {mean_population}")
     logger.info(f"The standard deviation is {std_population}")
 
-    ### 3.2 Using the dataframe from the time-series (Part 1), For every series_id, find the best year: the year with the max/largest sum of "value" for all quarters in that year. Generate a report with each series id, the best year for that series, and the summed value for that year. For example, if the table had the following values:
+    # 3.2 Using the dataframe from the time-series (Part 1), For every series_id,
+    # find the best year: the year with the max/largest sum of "value" 
+    # for all quarters in that year. Generate a report with each series id, 
+    # the best year for that series, and the summed value for that year. 
 
     df = df_part1.groupby(["series_id", "year"])["value"].sum()
     df = df.reset_index()
@@ -78,7 +84,10 @@ def handler(event, context):
     logger.info("The best year for every series id:")
     logger.info(f"\n {df_result.to_markdown(floatfmt='.1f')}")
 
-    ### 3.3 Using both dataframes from Part 1 and Part 2, generate a report that will provide the value for series_id = PRS30006032 and period = Q01 and the population for that given year (if available in the population dataset)
+    ### 3.3 Using both dataframes from Part 1 and Part 2, 
+    # generate a report that will provide the value for series_id = PRS30006032 
+    # and period = Q01 and the population for that given year 
+    # (if available in the population dataset)
 
     df_1 = df_part1[["series_id", "year", "period", "value"]]
     df_2 = df_part2[["Year", "Population"]]
@@ -91,7 +100,8 @@ def handler(event, context):
     df_report = df_report.reset_index(drop=True)
 
     logger.info(
-        "The report that will provide the value for series_id = PRS30006032 and period = Q01 and the population for that given year"
+        """The report that will provide the value for series_id = PRS30006032 
+        and period = Q01 and the population for that given year"""
     )
     logger.info(f"\n {df_report.to_markdown(floatfmt='.1f')}")
 
--- test_scripts/s3_boto_test.py
+++
@@ -1,4 +0,0 @@
-import boto3
-s3 = boto3.client('s3')
-
-print(s3.list_objects(Bucket="rearc-quest-bucket-roman"))
